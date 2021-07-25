56b0834de24cc89543b829eee8797df2ee42b91e
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-07-24 23:59:03 -0300

    feat: Main mapeado para AsyncMain()

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ namespace JdB_Bot
     class Program
     {
         static void Main(string[] args)
+            => new Program().AsyncMain().GetAwaiter().GetResult();
+
+        public async Task AsyncMain()
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
