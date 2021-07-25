25483efeeb4bcdacf0068e88a69cebe2b5847ace
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-07-25 15:28:38 -0300

    feat: implementando a API, contexto async e logs
    BREAKING CHANGE: Main() reprogramado para AsyncMain()

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
 
 namespace JdB_Bot
 {
@@ -7,8 +10,25 @@ namespace JdB_Bot
         static void Main(string[] args)
             => new Program().AsyncMain().GetAwaiter().GetResult();
 
+        private DiscordSocketClient DSClient;
+
         public async Task AsyncMain()
         {
+            DSClient = new DiscordSocketClient();
+            DSClient.Log += Log;
+
+            var token = Environment.GetEnvironmentVariable("DiscordToken");
+
+            await DSClient.LoginAsync(TokenType.Bot, token);
+            await DSClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
