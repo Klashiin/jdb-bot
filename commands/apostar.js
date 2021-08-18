37c362e4275fc8d789dc0fd4439a082ffba265ea
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-17 21:05:07 -0300

    feat: protótipo de tratamento a numero aleatorio gerado em 'apostar.js'

--- commands/apostar.js
+++ commands/apostar.js
@@ -9,9 +9,22 @@ module.exports = {
     console.log(quantia);
     console.log(bicho);
     // Checagens:
+    if (!args.length)
+      return message.reply(
+        "Você precisa inserir uma quantia para usar esse comando!"
+      );
     if (quantia % 1 != 0 || quantia <= 0)
       return message.reply(
         "A quantia a ser paga tem que ser um número inteiro positivo."
       );
+    // RANDOM NUMVERZZZ
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    const randonNumber = getRandomInt(1000, 9999);
+    console.log(randomNumber);
+    console.log(randomNumber.toString().substr(-2));
   },
 };
