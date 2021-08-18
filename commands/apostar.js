723a55e1631db656cd58aa02652fc520393f94ef
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-18 14:08:21 -0300

    feat: imagens de cada bicho adicionadas em 'apostar.js'

--- commands/apostar.js
+++ commands/apostar.js
@@ -57,7 +57,7 @@ module.exports = {
         allow = true;
       }
     }
-    if (!allow) {
+    if (allow === false) {
       return message.reply("Por favor, insira um bicho válido!");
     }
     // Se passar as checagens, subtrair a quantia
@@ -84,61 +84,99 @@ module.exports = {
     // Definir bichoSorteado baseado na dezena:
     if (dezena >= 01 && dezena <= 04) {
       bichoSorteado = "avestruz";
+      imagem =
+        "https://media0.giphy.com/media/4JSAeVRSI8VG5lGtiT/giphy.gif?cid=e1bb72ff5c456096774449594159160b";
     } else if (dezena >= 05 && dezena <= 08) {
       bichoSorteado = "águia";
+      imagem = "https://c.tenor.com/hFNobzSfOZsAAAAC/%C3%A1guia-eagle.gif";
     } else if (dezena >= 09 && dezena <= 12) {
       bichoSorteado = "burro";
+      imagem = "https://c.tenor.com/XTiqQYf6f_UAAAAM/no-donkeys.gif";
     } else if (dezena >= 13 && dezena <= 16) {
       bichoSorteado = "borboleta";
+      imagem = "https://media2.giphy.com/media/taIN8qu0tqMw/giphy.gif";
     } else if (dezena >= 17 && dezena <= 20) {
       bichoSorteado = "cachorro";
+      imagem = "https://images.gamebanana.com/img/ico/sprays/61118a0b23ab7.gif";
     } else if (dezena >= 21 && dezena <= 24) {
       bichoSorteado = "cabra";
+      imagem =
+        "https://66.media.tumblr.com/61616edea0360f1b8f91177ad3824ba8/180510d851ab8c9b-fa/s250x400/4f6f5fb5697f75f23ca0145abd6be3a58802544a.gif";
     } else if (dezena >= 25 && dezena <= 28) {
       bichoSorteado = "carneiro";
+      imagem =
+        "https://i.pinimg.com/originals/8c/51/c4/8c51c45de0b90ce5483f31aa305de1d8.gif";
     } else if (dezena >= 29 && dezena <= 32) {
       bichoSorteado = "camelo";
+      imagem = "https://media2.giphy.com/media/4W8L0WVcwBEnS/200w.gif";
     } else if (dezena >= 33 && dezena <= 36) {
       bichoSorteado = "cobra";
+      imagem =
+        "https://i.pinimg.com/originals/9a/9c/e4/9a9ce43049590cee3c9e011dfcea78f5.gif";
     } else if (dezena >= 37 && dezena <= 40) {
       bichoSorteado = "coelho";
+      imagem =
+        "https://i.pinimg.com/originals/91/8f/1b/918f1b2c568be3d77a7c29d682be874c.gif";
     } else if (dezena >= 41 && dezena <= 44) {
       bichoSorteado = "cavalo";
+      imagem =
+        "https://i.pinimg.com/originals/36/f2/41/36f24141bc15986e09e7a3f75893bd33.gif";
     } else if (dezena >= 45 && dezena <= 48) {
       bichoSorteado = "elefante";
+      imagem =
+        "https://i.pinimg.com/originals/d3/a9/33/d3a933d1771418b826c970f4c95ce6f3.gif";
     } else if (dezena >= 49 && dezena <= 52) {
       bichoSorteado = "galo";
+      imagem =
+        "https://i.pinimg.com/originals/c1/1f/92/c11f92f246706adf9ebce8f74a612e92.gif";
     } else if (dezena >= 53 && dezena <= 56) {
       bichoSorteado = "gato";
+      imagem = "https://media4.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif";
     } else if (dezena >= 57 && dezena <= 60) {
       bichoSorteado = "jacaré";
+      imagem = "https://thumbs.gfycat.com/GlassAgitatedKudu-max-1mb.gif";
     } else if (dezena >= 61 && dezena <= 64) {
       bichoSorteado = "leão";
+      imagem = "https://media0.giphy.com/media/FGbIgRIqSrsIw/giphy.gif";
     } else if (dezena >= 65 && dezena <= 68) {
       bichoSorteado = "macaco";
+      imagem = "https://c.tenor.com/jHif6boX0hAAAAAM/shocked-shook.gif";
     } else if (dezena >= 69 && dezena <= 72) {
       bichoSorteado = "porco";
+      imagem = "https://c.tenor.com/aGVLdbYSFngAAAAM/cute-pig-polite-pig.gif";
     } else if (dezena >= 73 && dezena <= 76) {
       bichoSorteado = "pavão";
+      imagem = "https://c.tenor.com/G5z-j_3CwSYAAAAC/peacock-bird.gif";
     } else if (dezena >= 77 && dezena <= 80) {
       bichoSorteado = "peru";
+      imagem =
+        "https://c.tenor.com/0RMAhpnpJrsAAAAC/big-neck-dancing-turkey.gif";
     } else if (dezena >= 81 && dezena <= 84) {
       bichoSorteado = "touro";
+      imagem = "https://c.tenor.com/8QNIw_1FdSQAAAAM/pica-pau-touro.gif";
     } else if (dezena >= 85 && dezena <= 88) {
       bichoSorteado = "tigre";
+      imagem =
+        "http://78.media.tumblr.com/2b1c51b175ab38be17cc6f8f44338df3/tumblr_oz359wVzHP1wedvnko1_500.gif";
     } else if (dezena >= 89 && dezena <= 92) {
       bichoSorteado = "urso";
+      imagem = "http://media.tumblr.com/tumblr_m9e1ekm8db1qkm21w.gif";
     } else if (dezena >= 93 && dezena <= 96) {
       bichoSorteado = "veado";
+      imagem =
+        "https://thumbs.gfycat.com/AngryTenderGuineafowl-size_restricted.gif";
     } else if ((dezena >= 97 && dezena <= 99) || dezena == 00) {
       bichoSorteado = "vaca";
+      imagem =
+        "https://i.pinimg.com/originals/0a/35/61/0a356142c7184ae283480e277bf81dda.gif";
     }
     // Exibir um embed com o bicho vencedor
     const resultEmbed = new Discord.MessageEmbed()
-      .setTitle("RESULTADO DO BICHO")
+      .setTitle("Resultado")
       .setColor("#f54272")
+      .setImage(imagem)
       .addFields({
-        name: `${bichoSorteado}`,
+        name: `${bichoSorteado.toString().toUpperCase()}`,
         value: `Aposta x18`,
       });
     message.channel.send(resultEmbed);
