ff416634570f4acbf51f771a3a9290cc31a09925
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-17 23:03:00 -0300

    feat: apostar.js finalizado v1.0.0

--- commands/apostar.js
+++ commands/apostar.js
@@ -1,8 +1,10 @@
+const profileModel = require("../models/profileSchema");
+
 module.exports = {
   name: "apostar",
   description:
     "~apostar [quantia] [bicho] : aposta x pontos no bicho especificado.",
-  execute(client, message, args, Discord, profileData) {
+  async execute(client, message, args, Discord, profileData) {
     // Definindo os argumentos:
     const quantia = args[0];
     const bicho = args[1];
@@ -24,7 +26,88 @@ module.exports = {
       return Math.floor(Math.random() * (max - min)) + min;
     }
     const randomNumber = getRandomInt(1000, 9999);
+    const dezena = randomNumber.toString().substr(-2);
     console.log(randomNumber);
-    console.log(randomNumber.toString().substr(-2));
+    console.log(dezena);
+    // Definir bichoSorteado baseado na dezena:
+    if (dezena >= 01 && dezena <= 04) {
+      bichoSorteado = "avestruz";
+    } else if (dezena >= 05 && dezena <= 08) {
+      bichoSorteado = "águia";
+    } else if (dezena >= 09 && dezena <= 12) {
+      bichoSorteado = "burro";
+    } else if (dezena >= 13 && dezena <= 16) {
+      bichoSorteado = "borboleta";
+    } else if (dezena >= 17 && dezena <= 20) {
+      bichoSorteado = "cachorro";
+    } else if (dezena >= 21 && dezena <= 24) {
+      bichoSorteado = "cabra";
+    } else if (dezena >= 25 && dezena <= 28) {
+      bichoSorteado = "carneiro";
+    } else if (dezena >= 29 && dezena <= 32) {
+      bichoSorteado = "camelo";
+    } else if (dezena >= 33 && dezena <= 36) {
+      bichoSorteado = "cobra";
+    } else if (dezena >= 37 && dezena <= 40) {
+      bichoSorteado = "coelho";
+    } else if (dezena >= 41 && dezena <= 44) {
+      bichoSorteado = "cavalo";
+    } else if (dezena >= 45 && dezena <= 48) {
+      bichoSorteado = "elefante";
+    } else if (dezena >= 49 && dezena <= 52) {
+      bichoSorteado = "galo";
+    } else if (dezena >= 53 && dezena <= 56) {
+      bichoSorteado = "gato";
+    } else if (dezena >= 57 && dezena <= 60) {
+      bichoSorteado = "jacaré";
+    } else if (dezena >= 61 && dezena <= 64) {
+      bichoSorteado = "leão";
+    } else if (dezena >= 65 && dezena <= 68) {
+      bichoSorteado = "macaco";
+    } else if (dezena >= 69 && dezena <= 72) {
+      bichoSorteado = "porco";
+    } else if (dezena >= 73 && dezena <= 76) {
+      bichoSorteado = "pavão";
+    } else if (dezena >= 77 && dezena <= 80) {
+      bichoSorteado = "peru";
+    } else if (dezena >= 81 && dezena <= 84) {
+      bichoSorteado = "touro";
+    } else if (dezena >= 85 && dezena <= 88) {
+      bichoSorteado = "tigre";
+    } else if (dezena >= 89 && dezena <= 92) {
+      bichoSorteado = "urso";
+    } else if (dezena >= 93 && dezena <= 96) {
+      bichoSorteado = "veado";
+    } else if ((dezena >= 97 && dezena <= 99) || dezena == 00) {
+      bichoSorteado = "vaca";
+    }
+    // Exibir um embed com o bicho vencedor
+    const resultEmbed = new Discord.MessageEmbed()
+      .setTitle("RESULTADO DO BICHO")
+      .setColor("#f54272")
+      .addFields({
+        name: `${bichoSorteado}`,
+        value: `Aposta x18`,
+      });
+    // Comparar bicho com bichoSorteado e recompensar se forem iguais:
+    if (bicho == bichoSorteado) {
+      await profileModel.findOneAndUpdate(
+        {
+          userID: message.author.id,
+        },
+        {
+          $inc: {
+            pontos: quantia * 18,
+          },
+        }
+      );
+      return message.reply(
+        `Você acertou o bicho e ganhou ${quantia * 18} pontos.`
+      );
+    } else {
+      return message.reply(
+        `O bicho escolhido (${bicho}) não foi sorteado dessa vez...`
+      );
+    }
   },
 };
