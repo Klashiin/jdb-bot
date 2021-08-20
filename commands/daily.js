60ffbfdd62512a7ba1be642040cead4e14dd13c9
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-20 14:14:03 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHH

--- commands/daily.js
+++ commands/daily.js
@@ -5,57 +5,56 @@ module.exports = {
   description: "Coleta o bônus diário (se disponível).",
   async execute(client, message, args, Discord, profileData) {
     for (let i = 1; i > 0; i++) {
-      if (!profileData) {
+      if (profileData.userID === null) {
         console.log("perfil gerado");
-      }
-    }
-
-    if (profileData) {
-      const cooldown = 86400000;
-      let lastDaily = profileData.lastDaily;
-      if (lastDaily + cooldown <= Date.now()) {
-        console.log("true");
-        // Definindo o bônus diário:
-        let bonus;
-        if (profileData.dailyStreak >= 10) {
-          bonus = 5 * 250;
-        } else {
-          bonus = (profileData.dailyStreak / 2) * 250;
-        }
-        // Dando os pontos pro usuário:
-        await profileModel.findOneAndUpdate(
-          {
-            userID: message.author.id,
-          },
-          {
-            $inc: {
-              pontos: bonus,
-            },
-          }
-        );
-        // Exibindo isso num embed bonitinho:
-        const dailyEmbed = new Discord.MessageEmbed()
-          .setTitle("Bônus Diário")
-          .addFields({
-            name: `${bonus} pontos`,
-            value: `<@${message.author.id}> está numa sequência de: ${profileData.dailyStreak}!`,
-          })
-          .setColor("#f54272");
-        message.channel.send(dailyEmbed);
-        // Atualizar lastDaily:
-        await profileModel.findOneAndUpdate(
-          {
-            userID: message.author.id,
-          },
-          {
-            $inc: {
-              lastDaily: Date.now(),
-            },
-          }
-        );
       } else {
-        console.log("false");
-        message.reply("seu bônus diário já foi coletado hoje.");
+        const cooldown = 86400000;
+        let lastDaily = profileData.lastDaily;
+        if (lastDaily + cooldown <= Date.now()) {
+          console.log("true");
+          // Definindo o bônus diário:
+          let bonus;
+          if (profileData.dailyStreak >= 10) {
+            bonus = 5 * 250;
+          } else {
+            bonus = (profileData.dailyStreak / 2) * 250;
+          }
+          // Dando os pontos pro usuário:
+          await profileModel.findOneAndUpdate(
+            {
+              userID: message.author.id,
+            },
+            {
+              $inc: {
+                pontos: bonus,
+              },
+            }
+          );
+          // Exibindo isso num embed bonitinho:
+          const dailyEmbed = new Discord.MessageEmbed()
+            .setTitle("Bônus Diário")
+            .addFields({
+              name: `${bonus} pontos`,
+              value: `<@${message.author.id}> está numa sequência de: ${profileData.dailyStreak}!`,
+            })
+            .setColor("#f54272");
+          message.channel.send(dailyEmbed);
+          // Atualizar lastDaily:
+          await profileModel.findOneAndUpdate(
+            {
+              userID: message.author.id,
+            },
+            {
+              $inc: {
+                lastDaily: Date.now(),
+              },
+            }
+          );
+        } else {
+          console.log("false");
+          message.reply("seu bônus diário já foi coletado hoje.");
+        }
+        i = 0;
       }
     }
   },
