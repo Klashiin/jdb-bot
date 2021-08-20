279da4ead82939cb58bfaddfbcc4e86995afca55
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-20 16:53:46 -0300

    listabichos.js - feat: adicionados emojis de todos os animais no campo value do embed

--- commands/listabichos.js
+++ commands/listabichos.js
@@ -3,47 +3,37 @@ module.exports = {
   description: "Exibe uma lista com todos os bichos do JdB.",
   execute(client, message, args, Discord, profileData) {
     const allowedBichos = [
-      "avestruz",
-      "águia",
-      "burro",
-      "borboleta",
-      "cachorro",
-      "cabra",
-      "carneiro",
-      "camelo",
-      "cobra",
-      "coelho",
-      "cavalo",
-      "elefante",
-      "galo",
-      "gato",
-      "jacaré",
-      "leão",
-      "macaco",
-      "porco",
-      "pavão",
-      "peru",
-      "touro",
-      "tigre",
-      "urso",
-      "veado",
-      "vaca",
+      {name: "Avestruz", value: ":bird: :bird: :bird:"},
+      {name: "Águia", value: ":eagle: :eagle: :eagle:"},
+      {name: "Burro", value: ":horse: :horse: :horse:"},
+      {name: "Borboleta", value: ":butterfly: :butterfly: :butterfly:"},
+      {name: "Cachorro", value: ":dog: :dog: :dog:"},
+      {name: "Cabra", value: ":goat: :goat: :goat:"},
+      {name: "Carneiro", value: ":ram: :ram: :ram:"},
+      {name: "Camelo", value: ":camel: :camel: :camel:"},
+      {name: "Cobra", value: ":snake: :snake: :snake:"},
+      {name: "Coelho", value: ":rabbit: :rabbit: :rabbit:"},
+      {name: "Cavalo", value: ":horse: :horse: :horse:"},
+      {name: "Elefante", value: ":elephant: :elephant: :elephant:"},
+      {name: "Galo", value: ":rooster: :rooster: :rooster:"},
+      {name: "Gato", value: ":cat2: :cat2: :cat2:"},
+      {name: "Jacaré", value: ":crocodile: :crocodile: :crocodile:"},
+      {name: "Leão", value: ":lion: :lion: :lion:"},
+      {name: "Macaco", value: ":monkey: :monkey: :monkey:"},
+      {name: "Porco", value: ":pig:: :pig: :pig:"},
+      {name: "Pavão", value: ":peacock:: :peacock: :peacock:"},
+      {name: "Peru", value: ":turkey:: :turkey: :turkey:"},
+      {name: "Touro", value: ":ox:: :ox: :ox:"},
+      {name: "Tigre", value: ":tiger:: :tiger: :tiger:"},
+      {name: "Urso", value: ":bear:: :bear: :bear:"},
+      {name: "Veado", value: ":deer:: :deer: :deer:"},
+      {name: "Vaca", value: ":cow:: :cow: :cow:"},
     ];
-
-    function createObj() {
-      let obj = [];
-      let i = 0;
-      for (const bicho of allowedBichos) {
-        obj[i] = {name: bicho.toUpperCase(), value: ":cowboy: :cowboy: :cowboy:"};
-        i++;
-      }
-      return obj;
-    }
 
     const listaEmbed = new Discord.MessageEmbed()
       .setTitle("Lista de Bichos")
       .setColor("#5865F2")
-      .addFields(createObj());
+      .addFields(allowedBichos);
     message.channel.send(listaEmbed);
   },
 };
