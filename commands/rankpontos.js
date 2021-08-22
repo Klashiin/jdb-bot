4f9e420325f9b423998121f61742777c61b70dec
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-22 00:07:22 -0300

    rankpontos.js - criado

---
+++ commands/rankpontos.js
@@ -0,0 +1,63 @@
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+  name: "rankpontos",
+  description: "Exibe um ranking dos membros do servidor, baseado nos pontos.",
+  execute(client, message, args, Discord, profileData) {
+    profileModel
+      .find({serverID: message.guild.id})
+      .sort([["pontos", "descending"]])
+      .exec((err, res) => {
+        if (err) console.log(err);
+
+        let rankEmbed = new Discord.MessageEmbed().setTitle(
+          "Ranking de Pontos"
+        );
+        if (res.length === 0) {
+          // se não tiverem resultados
+          rankEmbed.setColor("RED");
+          rankEmbed.addField(
+            "Nenhum dado encontrado",
+            "Ganhe pontos apostando ou pegando sua daily!"
+          );
+        } else if (res.length < 10) {
+          // se tiverem menos de 10 resultados
+          rankEmbed.setColor("#5865F2");
+          for (i = 0; i < res.length; i++) {
+            let member =
+              message.guild.members.get(res[i].userID) || "User Left";
+            if (member === "User Left") {
+              rankEmbed.addField(
+                `${i + 1}. ${member}`,
+                `${res[i].pontos} pontos`
+              );
+            } else {
+              rankEmbed.addField(
+                `${i + 1}. ${member.user.username}`,
+                `${res[i].pontos} pontos`
+              );
+            }
+          }
+        } else {
+          // se tiverem mais de 10 resultados
+          rankEmbed.setColor("#5865F2");
+          for (i = 0; i < 10; i++) {
+            let member =
+              message.guild.members.get(res[i].userID) || "User Left";
+            if (member === "User Left") {
+              rankEmbed.addField(
+                `${i + 1}. ${member}`,
+                `${res[i].pontos} pontos`
+              );
+            } else {
+              rankEmbed.addField(
+                `${i + 1}. ${member.user.username}`,
+                `${res[i].pontos} pontos`
+              );
+            }
+          }
+        }
+        message.channel.send(rankEmbed);
+      });
+  },
+};
