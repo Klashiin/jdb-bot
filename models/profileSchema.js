91c0fadf59ab71f1bd24767d27f9ed9daea2b04d
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-24 00:07:37 -0300

    profileSchema.js - fix: valor padrão de wins e losses acertado para '1'

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -6,8 +6,8 @@ const profileSchema = new mongoose.Schema({
   pontos: {type: Number, default: 1000},
   lastDaily: {type: Number, default: 0},
   dailyStreak: {type: Number, default: 1},
-  wins: {type: Number, require: true},
-  losses: {type: Number, require: true},
+  wins: {type: Number, require: true, default: 1},
+  losses: {type: Number, require: true, default: 1},
 });
 
 const model = mongoose.model("ProfileModels", profileSchema);
