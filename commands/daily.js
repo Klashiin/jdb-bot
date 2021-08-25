136b1c86c32fa68cec2f8ddcbbdea8eff1043122
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-25 17:19:35 -0300

    daily.js - feat: adicionado incremento a dailyStreak caso daily for coletado, readme.html - criado

--- commands/daily.js
+++ commands/daily.js
@@ -47,6 +47,17 @@ module.exports = {
             },
           }
         );
+        // Aumentando a streak do usuário:
+        await profileModel.findOneAndUpdate(
+          {
+            userID: message.author.id,
+          },
+          {
+            $inc: {
+              dailyStreak: 1,
+            },
+          }
+        );
       } else {
         console.log("false");
         message.reply("seu bônus diário já foi coletado hoje.");
