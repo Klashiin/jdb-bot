491e9be6e0fa566cc025e8ec7bd5443a7434eb5d
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-25 17:13:15 -0300

    Revert "aposta,js - feat: tentei tornar aposta em um comando que gera 5 números aleatórios mas falhei miseravelmente"
    This reverts commit 1d40e71f34df3f47e62246fd56a7c0f24ba42bc1.
    
    "apostar.js revertido"

--- commands/apostar.js
+++ commands/apostar.js
@@ -84,130 +84,152 @@ module.exports = {
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min)) + min;
     }
-    let bichosSorteados = [];
-    let randomNumbers = [];
-    for (i = 0; i < 5; i++) {
-      randomNumbers[i] = getRandomInt(1000, 9999);
-      let dezena = randomNumbers[i].toString().substr(-2);
-      console.log(randomNumber);
-      console.log(dezena);
-      // Definir bichosSorteados baseado na dezena:
-      if (dezena >= 01 && dezena <= 04) {
-        bichosSorteados[i] = "avestruz";
-        imagem =
-          "https://media0.giphy.com/media/4JSAeVRSI8VG5lGtiT/giphy.gif?cid=e1bb72ff5c456096774449594159160b";
-        emoji = ":bird:";
-      } else if (dezena >= 05 && dezena <= 08) {
-        bichosSorteados[i] = "águia";
-        imagem = "https://c.tenor.com/hFNobzSfOZsAAAAC/%C3%A1guia-eagle.gif";
-        emoji = ":eagle:";
-      } else if (dezena >= 09 && dezena <= 12) {
-        bichosSorteados[i] = "burro";
-        imagem = "https://c.tenor.com/XTiqQYf6f_UAAAAM/no-donkeys.gif";
-        emoji = ":horse:";
-      } else if (dezena >= 13 && dezena <= 16) {
-        bichosSorteados[i] = "borboleta";
-        imagem = "https://media2.giphy.com/media/taIN8qu0tqMw/giphy.gif";
-        emoji = ":butterfly:";
-      } else if (dezena >= 17 && dezena <= 20) {
-        bichosSorteados[i] = "cachorro";
-        imagem =
-          "https://images.gamebanana.com/img/ico/sprays/61118a0b23ab7.gif";
-        emoji = ":dog:";
-      } else if (dezena >= 21 && dezena <= 24) {
-        bichosSorteados[i] = "cabra";
-        imagem =
-          "https://66.media.tumblr.com/61616edea0360f1b8f91177ad3824ba8/180510d851ab8c9b-fa/s250x400/4f6f5fb5697f75f23ca0145abd6be3a58802544a.gif";
-        emoji = ":goat:";
-      } else if (dezena >= 25 && dezena <= 28) {
-        bichosSorteados[i] = "carneiro";
-        imagem =
-          "https://i.pinimg.com/originals/8c/51/c4/8c51c45de0b90ce5483f31aa305de1d8.gif";
-        emoji = ":ram:";
-      } else if (dezena >= 29 && dezena <= 32) {
-        bichosSorteados[i] = "camelo";
-        imagem =
-          "https://pa1.narvii.com/6356/adc9bf9a2d591e19e8d1c596fd557269a95d5a77_hq.gif";
-        emoji = ":camel:";
-      } else if (dezena >= 33 && dezena <= 36) {
-        bichosSorteados[i] = "cobra";
-        imagem =
-          "https://i.pinimg.com/originals/9a/9c/e4/9a9ce43049590cee3c9e011dfcea78f5.gif";
-        emoji = ":snake:";
-      } else if (dezena >= 37 && dezena <= 40) {
-        bichosSorteados[i] = "coelho";
-        imagem =
-          "https://i.pinimg.com/originals/91/8f/1b/918f1b2c568be3d77a7c29d682be874c.gif";
-        emoji = ":rabbit:";
-      } else if (dezena >= 41 && dezena <= 44) {
-        bichosSorteados[i] = "cavalo";
-        imagem =
-          "https://i.pinimg.com/originals/36/f2/41/36f24141bc15986e09e7a3f75893bd33.gif";
-        emoji = ":horse:";
-      } else if (dezena >= 45 && dezena <= 48) {
-        bichosSorteados[i] = "elefante";
-        imagem =
-          "https://i.pinimg.com/originals/d3/a9/33/d3a933d1771418b826c970f4c95ce6f3.gif";
-        emoji = ":elephant:";
-      } else if (dezena >= 49 && dezena <= 52) {
-        bichosSorteados[i] = "galo";
-        imagem =
-          "https://i.pinimg.com/originals/b0/f6/68/b0f6685e60fb207fd98644d876e46daa.gif";
-        emoji = ":rooster:";
-      } else if (dezena >= 53 && dezena <= 56) {
-        bichosSorteados[i] = "gato";
-        imagem = "https://media4.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif";
-        emoji = ":cat2:";
-      } else if (dezena >= 57 && dezena <= 60) {
-        bichosSorteados[i] = "jacaré";
-        imagem = "https://thumbs.gfycat.com/GlassAgitatedKudu-max-1mb.gif";
-        emoji = ":crocodile:";
-      } else if (dezena >= 61 && dezena <= 64) {
-        bichosSorteados[i] = "leão";
-        imagem = "https://media0.giphy.com/media/FGbIgRIqSrsIw/giphy.gif";
-        emoji = ":lion:";
-      } else if (dezena >= 65 && dezena <= 68) {
-        bichosSorteados[i] = "macaco";
-        imagem = "https://c.tenor.com/jHif6boX0hAAAAAM/shocked-shook.gif";
-        emoji = ":monkey:";
-      } else if (dezena >= 69 && dezena <= 72) {
-        bichosSorteados[i] = "porco";
-        imagem = "https://c.tenor.com/aGVLdbYSFngAAAAM/cute-pig-polite-pig.gif";
-        emoji = ":pig:";
-      } else if (dezena >= 73 && dezena <= 76) {
-        bichosSorteados[i] = "pavão";
-        imagem = "https://c.tenor.com/G5z-j_3CwSYAAAAC/peacock-bird.gif";
-        emoji = ":peacock:";
-      } else if (dezena >= 77 && dezena <= 80) {
-        bichosSorteados[i] = "peru";
-        imagem =
-          "https://c.tenor.com/0RMAhpnpJrsAAAAC/big-neck-dancing-turkey.gif";
-        emoji = ":turkey:";
-      } else if (dezena >= 81 && dezena <= 84) {
-        bichosSorteados[i] = "touro";
-        imagem = "https://c.tenor.com/8QNIw_1FdSQAAAAM/pica-pau-touro.gif";
-        emoji = ":ox:";
-      } else if (dezena >= 85 && dezena <= 88) {
-        bichosSorteados[i] = "tigre";
-        imagem =
-          "http://78.media.tumblr.com/2b1c51b175ab38be17cc6f8f44338df3/tumblr_oz359wVzHP1wedvnko1_500.gif";
-        emoji = ":tiger:";
-      } else if (dezena >= 89 && dezena <= 92) {
-        bichosSorteados[i] = "urso";
-        imagem = "http://media.tumblr.com/tumblr_m9e1ekm8db1qkm21w.gif";
-        emoji = ":bear:";
-      } else if (dezena >= 93 && dezena <= 96) {
-        bichosSorteados[i] = "veado";
-        imagem =
-          "https://thumbs.gfycat.com/AngryTenderGuineafowl-size_restricted.gif";
-        emoji = ":deer:";
-      } else if ((dezena >= 97 && dezena <= 99) || dezena == 00) {
-        bichosSorteados[i] = "vaca";
-        imagem =
-          "https://i.pinimg.com/originals/0a/35/61/0a356142c7184ae283480e277bf81dda.gif";
-        emoji = ":cow:";
-      }
+    const randomNumber = getRandomInt(1000, 9999);
+    const dezena = randomNumber.toString().substr(-2);
+    console.log(randomNumber);
+    console.log(dezena);
+    // Definir bichoSorteado baseado na dezena:
+    if (dezena >= 01 && dezena <= 04) {
+      bichoSorteado = "avestruz";
+      imagem =
+        "https://media0.giphy.com/media/4JSAeVRSI8VG5lGtiT/giphy.gif?cid=e1bb72ff5c456096774449594159160b";
+      emoji = ":bird:";
+    } else if (dezena >= 05 && dezena <= 08) {
+      bichoSorteado = "águia";
+      imagem = "https://c.tenor.com/hFNobzSfOZsAAAAC/%C3%A1guia-eagle.gif";
+      emoji = ":eagle:";
+    } else if (dezena >= 09 && dezena <= 12) {
+      bichoSorteado = "burro";
+      imagem = "https://c.tenor.com/XTiqQYf6f_UAAAAM/no-donkeys.gif";
+      emoji = ":horse:";
+    } else if (dezena >= 13 && dezena <= 16) {
+      bichoSorteado = "borboleta";
+      imagem = "https://media2.giphy.com/media/taIN8qu0tqMw/giphy.gif";
+      emoji = ":butterfly:";
+    } else if (dezena >= 17 && dezena <= 20) {
+      bichoSorteado = "cachorro";
+      imagem = "https://images.gamebanana.com/img/ico/sprays/61118a0b23ab7.gif";
+      emoji = ":dog:";
+    } else if (dezena >= 21 && dezena <= 24) {
+      bichoSorteado = "cabra";
+      imagem =
+        "https://66.media.tumblr.com/61616edea0360f1b8f91177ad3824ba8/180510d851ab8c9b-fa/s250x400/4f6f5fb5697f75f23ca0145abd6be3a58802544a.gif";
+      emoji = ":goat:";
+    } else if (dezena >= 25 && dezena <= 28) {
+      bichoSorteado = "carneiro";
+      imagem =
+        "https://i.pinimg.com/originals/8c/51/c4/8c51c45de0b90ce5483f31aa305de1d8.gif";
+      emoji = ":ram:";
+    } else if (dezena >= 29 && dezena <= 32) {
+      bichoSorteado = "camelo";
+      imagem =
+        "https://pa1.narvii.com/6356/adc9bf9a2d591e19e8d1c596fd557269a95d5a77_hq.gif";
+      emoji = ":camel:";
+    } else if (dezena >= 33 && dezena <= 36) {
+      bichoSorteado = "cobra";
+      imagem =
+        "https://i.pinimg.com/originals/9a/9c/e4/9a9ce43049590cee3c9e011dfcea78f5.gif";
+      emoji = ":snake:";
+    } else if (dezena >= 37 && dezena <= 40) {
+      bichoSorteado = "coelho";
+      imagem =
+        "https://i.pinimg.com/originals/91/8f/1b/918f1b2c568be3d77a7c29d682be874c.gif";
+      emoji = ":rabbit:";
+    } else if (dezena >= 41 && dezena <= 44) {
+      bichoSorteado = "cavalo";
+      imagem =
+        "https://i.pinimg.com/originals/36/f2/41/36f24141bc15986e09e7a3f75893bd33.gif";
+      emoji = ":horse:";
+    } else if (dezena >= 45 && dezena <= 48) {
+      bichoSorteado = "elefante";
+      imagem =
+        "https://i.pinimg.com/originals/d3/a9/33/d3a933d1771418b826c970f4c95ce6f3.gif";
+      emoji = ":elephant:";
+    } else if (dezena >= 49 && dezena <= 52) {
+      bichoSorteado = "galo";
+      imagem =
+        "https://i.pinimg.com/originals/b0/f6/68/b0f6685e60fb207fd98644d876e46daa.gif";
+      emoji = ":rooster:";
+    } else if (dezena >= 53 && dezena <= 56) {
+      bichoSorteado = "gato";
+      imagem = "https://media4.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif";
+      emoji = ":cat2:";
+    } else if (dezena >= 57 && dezena <= 60) {
+      bichoSorteado = "jacaré";
+      imagem = "https://thumbs.gfycat.com/GlassAgitatedKudu-max-1mb.gif";
+      emoji = ":crocodile:";
+    } else if (dezena >= 61 && dezena <= 64) {
+      bichoSorteado = "leão";
+      imagem = "https://media0.giphy.com/media/FGbIgRIqSrsIw/giphy.gif";
+      emoji = ":lion:";
+    } else if (dezena >= 65 && dezena <= 68) {
+      bichoSorteado = "macaco";
+      imagem = "https://c.tenor.com/jHif6boX0hAAAAAM/shocked-shook.gif";
+      emoji = ":monkey:";
+    } else if (dezena >= 69 && dezena <= 72) {
+      bichoSorteado = "porco";
+      imagem = "https://c.tenor.com/aGVLdbYSFngAAAAM/cute-pig-polite-pig.gif";
+      emoji = ":pig:";
+    } else if (dezena >= 73 && dezena <= 76) {
+      bichoSorteado = "pavão";
+      imagem = "https://c.tenor.com/G5z-j_3CwSYAAAAC/peacock-bird.gif";
+      emoji = ":peacock:";
+    } else if (dezena >= 77 && dezena <= 80) {
+      bichoSorteado = "peru";
+      imagem =
+        "https://c.tenor.com/0RMAhpnpJrsAAAAC/big-neck-dancing-turkey.gif";
+      emoji = ":turkey:";
+    } else if (dezena >= 81 && dezena <= 84) {
+      bichoSorteado = "touro";
+      imagem = "https://c.tenor.com/8QNIw_1FdSQAAAAM/pica-pau-touro.gif";
+      emoji = ":ox:";
+    } else if (dezena >= 85 && dezena <= 88) {
+      bichoSorteado = "tigre";
+      imagem =
+        "http://78.media.tumblr.com/2b1c51b175ab38be17cc6f8f44338df3/tumblr_oz359wVzHP1wedvnko1_500.gif";
+      emoji = ":tiger:";
+    } else if (dezena >= 89 && dezena <= 92) {
+      bichoSorteado = "urso";
+      imagem = "http://media.tumblr.com/tumblr_m9e1ekm8db1qkm21w.gif";
+      emoji = ":bear:";
+    } else if (dezena >= 93 && dezena <= 96) {
+      bichoSorteado = "veado";
+      imagem =
+        "https://thumbs.gfycat.com/AngryTenderGuineafowl-size_restricted.gif";
+      emoji = ":deer:";
+    } else if ((dezena >= 97 && dezena <= 99) || dezena == 00) {
+      bichoSorteado = "vaca";
+      imagem =
+        "https://i.pinimg.com/originals/0a/35/61/0a356142c7184ae283480e277bf81dda.gif";
+      emoji = ":cow:";
     }
+    // Adicionar bichoSorteado ao DB se não existir:
+    let bichoData;
+    try {
+      bichoData = await bichoModel.findOne({nome: bichoSorteado});
+      if (!bichoData) {
+        let novoBicho = await bichoModel.create({
+          nome: bichoSorteado,
+          image: imagem,
+          emoji: emoji,
+          totalWins: 0,
+        });
+        novoBicho.save();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    // E atualizar o totalWins dele:
+    await bichoModel.findOneAndUpdate(
+      {
+        nome: bichoSorteado,
+      },
+      {
+        $inc: {
+          totalWins: 1,
+        },
+      }
+    );
     // Exibir um embed com o bicho vencedor
     const resultEmbed = new Discord.MessageEmbed()
       .setTitle("Resultado")
