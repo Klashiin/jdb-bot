719c1073b858fca96c39b70ba8f8bb516dc3632f
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-08-26 17:28:45 -0300

    trabalhar.js - criado

---
+++ commands/trabalhar.js
@@ -0,0 +1,34 @@
+module.exports = {
+  name: "trabalhar",
+  description: "Ganha uma quantia aleatória de pontos.",
+  async execute(client, message, args, Discord, profileData) {
+    // Definir a quantia através de número aleatório
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    const randomNumber = getRandomInt(80, 250);
+    console.log(randomNumber);
+    // Dar os pontos para o usuário
+    await profileModel.findOneAndUpdate(
+      {
+        userID: message.author.id,
+      },
+      {
+        $inc: {
+          pontos: randomNumber,
+        },
+      }
+    );
+    // Exibir isso num embed bonitinho
+    const workEmbed = new Discord.MessageEmbed()
+      .setColor("#118C4F")
+      .setTitle("Resultado")
+      .addField(
+        `${profileData.name} trabalhou e recebeu seu pagamento!`,
+        `${randomNumber} pontos`
+      );
+    message.channel.send(workEmbed);
+  },
+};
